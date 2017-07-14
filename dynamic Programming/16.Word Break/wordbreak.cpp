a9a263e6643cbb483947f0439a5bc3807a7248ed
ajay kundu <ajaykunduktl2@gmail.com>
2017-07-14 19:14:36 +0530

    word break 2.0

--- dynamic Programming/16.Word Break/wordbreak.cpp
+++ dynamic Programming/16.Word Break/wordbreak.cpp
@@ -1,62 +1,46 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <cstring>
 using namespace std;
 
-bool checkindic(string str,string dic[],int sizedic){
-         
-        
-	for(int i=0;i<sizedic;i++)
-	{    
-		if(str.compare(dic[i])==0)
-			return true;
-	}
-	return false;
-}
-
-bool wordbreak(string str,int n,string dic[],int sizedic)
+bool wordBreak(string str,std::map<string,int>& map)
 {
-	bool dp[n+1];
+	int size=str.size();
+	bool dp[size+1];
+	
+	memset(dp,0,sizeof(dp));
 	dp[0]=true;
-
-	for(int i=1;i<n;i++)
+	for(int i=1;i<=size;i++)
 	{
-		int j=i-1;
-		for(;j>=0;j--)
+		for(int j=i-1;j>=0;j--)
 		{
-			if(dp[j]=true)
-			{
-				if(checkindic(str.substr(j,i-j),dic,sizedic))
-				{ 
-					cout<<str.substr(j,i-j)<<endl;
-					dp[i]=true;
-					break;
-				}
-			}
-		}
-		if(j==-1)
-		{
-			dp[i]==false;
+			
+			  if(dp[i]==false && dp[j]==true && map[str.substr(j,i-j)])
+			  { 
+			  	dp[i]=true;
+			  	
+			  }
 		}
 	}
-	for(int i=0;i<=n;i++)
-	{
-		cout<<dp[i]<<" ";
-	}
-   return dp[n];
+	
+	return dp[size];
 }
 
 int main(int argc, char const *argv[])
 {
 	string dictionary[]={"i","like","sum","sung","samsung","mobile","ice","cream","icecream","man","go","mango"};
-     
      int sizeofDictionary=sizeof(dictionary)/sizeof(dictionary[0]);
+     string str="icesungsum";
+    
+    std::map<string,int> map;
 
-     cout<<sizeofDictionary;
-     
-     string str="ilikesamsung";
-
-    cout<<endl;
-     cout<<wordbreak(str,str.length(),dictionary,sizeofDictionary);
+    for(int i=0;i<sizeofDictionary;i++)
+    {
+    	map[dictionary[i]]=1;
+    }
+    
+    cout<<wordBreak(str,map);
 
 	return 0;
 }

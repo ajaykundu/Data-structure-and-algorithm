38338a1a6d62d1003e3d716dd0c89607246e0241
ajay kundu <ajaykunduktl2@gmail.com>
2017-07-25 19:50:33 +0530

    reverseBtis using geeksforgeeks solution

---
+++ Bit Manipulation/reverseBits/RecerseBits.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+unsigned int reverseBits(unsigned int num)
+{
+    unsigned int  NO_OF_BITS = sizeof(num) * 8;
+    unsigned int reverse_num = 0, i, temp;
+ 
+    for (i = 0; i < NO_OF_BITS; i++)
+    {
+        temp = (num & (1 << i));
+        if(temp)
+            reverse_num |= (1 << ((NO_OF_BITS - 1) - i));
+    }
+  
+    return reverse_num;
+}
+ 
+/* Driver function to test above function */
+int main()
+{
+    unsigned int x = 1; 
+    cout<<reverseBits(x);
+}
